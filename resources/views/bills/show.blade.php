d2405651aa5e83864621719b3d3c75960a593ad2
Md Ashiqul Islam <ashique19@gmail.com>
2026-06-04 11:26:25 +0000

    Label update

--- resources/views/bills/show.blade.php
+++ resources/views/bills/show.blade.php
@@ -43,7 +43,8 @@
                             <th>Current Reading</th>
                             <th>Used Unit (m³)</th>
                             <th>Used Mass (kg)</th>
-                            <th class="pe-4 text-end">Amount Due</th>
+                            <th>Amount Due</th>
+                            <th class="pe-4 text-end">Status</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -57,14 +58,6 @@
                                     <a href="{{ route('flats.show', $detail->flat_id) }}" class="text-decoration-none font-weight-bold text-primary">
                                         Flat {{ $detail->flat->name ?? $detail->flat_id }} ↗
                                     </a>
-
-                                    <a href="{{ route('flats.bill.print', ['flat_id' => $detail->flat_id, 'bill_month' => $bill->bill_for_month]) }}" 
-                                        target="_blank" 
-                                        class="btn btn-sm btn-outline-secondary px-2 py-0.5"
-                                        style="font-size: 11px;">
-                                        📄 Print
-                                    </a>
-
                                 </td>
                                 <td>{{ number_format($detail->previous_reading, 2) }}</td>
                                 <td>{{ number_format($detail->current_reading, 2) }}</td>
